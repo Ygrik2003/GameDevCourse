b5dbf2f7a5d9a6f0a5a69d4bb9b97fc086d6ded6
Stepanov Igor <ygrik2003@yandex.ru>
2023-05-01 16:51:15 +0300

    add moving

--- 04-1-render-basic/3d_render_main.cpp
+++ 04-1-render-basic/3d_render_main.cpp
@@ -7,40 +7,59 @@
 #include <algorithm>
 #include <chrono>
 #include <iostream>
+#include <thread>
 
 constexpr int width  = 1000;
 constexpr int height = 1000;
 
 constexpr double R      = 100;
-constexpr double dTheta = 2 * M_PI / 50;
-constexpr double dPhi   = M_PI / 15;
+constexpr double dTheta = M_PI / 15;
+constexpr double dPhi   = 2 * M_PI / 15;
+
+void vertexs_projection(const std::vector<vertex>& v_in,
+                        std::vector<vertex>&       v_out,
+                        view_convertation&         view)
+{
+    v_out.resize(v_in.size());
+    for (int i = 0; i < v_in.size(); i++)
+    {
+        matrix res = view.convert(static_cast<int>(v_in[i].x),
+                                  static_cast<int>(v_in[i].y),
+                                  static_cast<int>(v_in[i].z));
+
+        v_out[i].x = width * (1 + res.getElement(0, 0)) / 2;
+        v_out[i].y = height * (1 + res.getElement(0, 1)) / 2;
+        v_out[i].z = 1 + res.getElement(0, 2);
+
+        v_out[i].r = v_in[i].r;
+        v_out[i].g = v_in[i].g;
+        v_out[i].b = v_in[i].b;
+        v_out[i].u = v_in[i].u;
+        v_out[i].v = v_in[i].v;
+    }
+}
 
 struct program : gfx_program
 {
-    void   set_uniforms(uniforms& uniforms_) { this->uniforms_1 = &uniforms_; }
+    void   set_uniforms(uniforms& uniforms_) { uniforms_1 = &uniforms_; }
     vertex vertex_shader(const vertex& v_in)
     {
         vertex out(v_in);
-
-        matrix res = view.convert(static_cast<int>(out.x),
-                                  static_cast<int>(out.y),
-                                  static_cast<int>(out.z));
-
-        out.x = width * (1 + res.getElement(0, 0)) / 2;
-        out.y = height * (1 + res.getElement(0, 1)) / 2;
-        out.z = 1 + res.getElement(0, 2);
-
         return out;
     }
     rgb fragment_shader(const vertex& v_in)
     {
+        double green =
+            255 * std::exp(-std::pow(std::pow(uniforms_1->f0 - v_in.x, 2) +
+                                         std::pow(uniforms_1->f1 - v_in.y, 2),
+                                     1. / 2.) /
+                           100);
+
         return { static_cast<uint8_t>(v_in.r),
-                 static_cast<uint8_t>(v_in.g),
+                 static_cast<uint8_t>(green),
                  static_cast<uint8_t>(v_in.b) };
     }
-    uniforms*         uniforms_1;
-    view_convertation view =
-        view_convertation(static_cast<double>(width) / height, 1, 3 * R);
+    uniforms* uniforms_1;
 } program_1;
 
 vertex get_sphere(double phi, double theta)
@@ -48,54 +67,88 @@ vertex get_sphere(double phi, double theta)
 
     return vertex{ R * (cos(phi) * sin(theta)),
                    R * (sin(phi) * sin(theta)),
-                   R * (2 + cos(theta)),
-                   255 * std::pow(std::cos(theta), 2),
-                   0,
-                   0,
-                   0,
-                   0 };
+                   R * (cos(theta)),
+                   255 * std::pow(std::cos(theta), 2) };
 }
 
 void get_sphere_triangles(std::vector<vertex>& vertexes,
                           std::vector<uint>&   indexes)
 {
+    auto get_index = [](int phi_c, int theta_c)
+    {
+        if (theta_c == 0)
+        {
+            return 0;
+        }
+        else if (theta_c == static_cast<int>(M_PI / dTheta) + 1)
+        {
+            std::cout << (static_cast<int>(M_PI / dTheta) + 1) *
+                                 (static_cast<int>(2 * M_PI / dPhi)) +
+                             1
+                      << std::endl;
+            return (static_cast<int>(M_PI / dTheta) + 1) *
+                       (static_cast<int>(2 * M_PI / dPhi)) +
+                   1;
+        }
+        else
+        {
 
-    int index = 0;
+            return 1 + (theta_c - 1) * static_cast<int>(2 * M_PI / dPhi + 1) +
+                   phi_c % static_cast<int>(2 * M_PI / dPhi);
+        }
+    };
 
+    int    index = 0;
     double theta = 0, phi;
 
-    for (int thetaCounter = 0; thetaCounter <= M_PI / dTheta; thetaCounter++)
+    vertexes.push_back(get_sphere(0, 0));
+    for (int theta_counter = 1; theta_counter <= M_PI / dTheta; theta_counter++)
     {
         phi = 0;
-        for (int phiCounter = 0; phiCounter <= 2 * M_PI / dPhi; phiCounter++)
+        for (int phi_counter = 0; phi_counter <= 2 * M_PI / dPhi; phi_counter++)
         {
             vertexes.push_back(get_sphere(phi, theta));
-            vertexes.push_back(get_sphere(phi, theta + dTheta));
-            vertexes.push_back(get_sphere(phi + dPhi, theta));
-            vertexes.push_back(get_sphere(phi + dPhi, theta + dTheta));
 
-            indexes.push_back(vertexes.size() - 4);
-            indexes.push_back(vertexes.size() - 3);
-            indexes.push_back(vertexes.size() - 2);
-            indexes.push_back(vertexes.size() - 3);
-            indexes.push_back(vertexes.size() - 2);
-            indexes.push_back(vertexes.size() - 1);
+            if (theta_counter < static_cast<int>(M_PI / dTheta))
+            {
+                indexes.push_back(get_index(phi_counter, theta_counter));
+                indexes.push_back(get_index(phi_counter + 1, theta_counter));
+                indexes.push_back(
+                    get_index(phi_counter + 1, theta_counter + 1));
 
+                indexes.push_back(get_index(phi_counter, theta_counter));
+                indexes.push_back(get_index(phi_counter, theta_counter + 1));
+                indexes.push_back(
+                    get_index(phi_counter + 1, theta_counter + 1));
+            }
+            // else
+            // {
+            //     indexes.push_back(get_index(phi_counter, theta_counter));
+            //     indexes.push_back(get_index(phi_counter + 1, theta_counter));
+            //     indexes.push_back(
+            //         get_index(phi_counter + 1, theta_counter + 1));
+            // }
             phi += dPhi;
         }
         theta += dTheta;
     }
+    vertexes.push_back(get_sphere(0, M_PI));
 }
 
 double mean_z(const std::vector<vertex>& vertexes, int i1, int i2, int i3)
 {
     std::vector<double> arr{ vertexes[i1].z, vertexes[i2].z, vertexes[i3].z };
-    return *std::max(arr.begin(), arr.end());
+    return *std::min(arr.begin(), arr.end());
     // return (vertexes[i1].z + vertexes[i2].z + vertexes[i3].z) / 3;
 }
 
 int main()
 {
+
+    int camera_x = 0;
+    int camera_y = 0;
+    int camera_z = 0;
+
     if (0 != SDL_Init(SDL_INIT_VIDEO))
     {
         std::cout << SDL_GetError() << std::endl;
@@ -104,7 +157,6 @@ int main()
 
     SDL_Window* window_main =
         SDL_CreateWindow("3D Render", width, height, SDL_WINDOW_OPENGL);
-
     if (window_main == nullptr)
     {
         std::cout << SDL_GetError() << std::endl;
@@ -113,7 +165,6 @@ int main()
 
     SDL_Renderer* renderer =
         SDL_CreateRenderer(window_main, nullptr, SDL_RENDERER_ACCELERATED);
-
     if (renderer == nullptr)
     {
         std::cout << SDL_GetError() << std::endl;
@@ -126,59 +177,81 @@ int main()
     const int pitch  = width * sizeof(rgb);
 
     triangle_interpolated_redner triangle_render(image);
-    triangle_render.set_pen_color(rgb{ 0, 0, 0 });
+    // triangle_render.set_pen_color(rgb{ 0, 0, 0 });
     triangle_render.clear(rgb{ 255, 255, 255 });
 
     uniforms uni;
-    double   phi = 0;
     program_1.set_uniforms(uni);
     triangle_render.set_gfx_program(program_1);
 
+    view_convertation   view(static_cast<double>(width) / height, 1, 3 * R);
+    double              phi = 0;
     std::vector<vertex> vertexes;
+    std::vector<vertex> current_vertexes;
     std::vector<uint>   indexes;
-
-    for (int i = 0; i < indexes.size(); i += 3)
-    {
-        for (int j = i + 3; j < indexes.size(); j += 3)
-        {
-            if (mean_z(
-                    vertexes, indexes[i + 0], indexes[i + 1], indexes[i + 2]) >
-                mean_z(
-                    vertexes, indexes[j + 0], indexes[j + 1], indexes[j + 2]))
-            {
-                std::swap(indexes[i + 0], indexes[j + 0]);
-                std::swap(indexes[i + 1], indexes[j + 1]);
-                std::swap(indexes[i + 2], indexes[j + 2]);
-            }
-        }
-    }
+    std::vector<uint>   current_indexes;
 
     get_sphere_triangles(vertexes, indexes);
 
-    bool                                       is_alive = true;
-    std::chrono::_V2::steady_clock::time_point last_time;
+    double mouse_x = 0, mouse_y = 0;
+    bool   is_alive = true;
     while (is_alive)
     {
         SDL_Event event;
         while (SDL_PollEvent(&event))
         {
             if (event.type == SDL_EVENT_QUIT)
+            {
                 is_alive = false;
-            break;
+                break;
+            }
+            else if (event.type == SDL_EVENT_MOUSE_MOTION)
+            {
+                uni.f0 = event.motion.x;
+                uni.f1 = event.motion.y;
+            }
+            else if (event.type == SDL_EVENT_KEY_DOWN)
+            {
+                if (event.key.keysym.sym == SDLK_w)
+                    camera_z -= 10;
+                if (event.key.keysym.sym == SDLK_s)
+                    camera_z += 10;
+                if (event.key.keysym.sym == SDLK_a)
+                    camera_x -= 10;
+                if (event.key.keysym.sym == SDLK_d)
+                    camera_x += 10;
+            }
         }
-        auto curr_time = std::chrono::steady_clock::now();
-        if (static_cast<std::chrono::duration<double>>(curr_time - last_time)
-                .count() < 1. / 60.)
-            continue;
-        last_time = curr_time;
 
-        program_1.view.set_translation(0, 0, 5 * R);
-        // program_1.view.set_translation(R * cos(phi), R * sin(phi), 50 * R);
-        program_1.view.set_rotation(0, 0, phi);
-        phi += dPhi / 5;
+        view.set_translation(
+            camera_x, camera_y + R * sin(phi), camera_z + 2 * R);
+        view.set_rotation(0, 0, phi);
+
+        vertexs_projection(vertexes, current_vertexes, view);
+        current_indexes = indexes;
+
+        for (int i = 0; i < current_indexes.size(); i += 3)
+        {
+            for (int j = i + 3; j < current_indexes.size(); j += 3)
+            {
+                if (mean_z(current_vertexes,
+                           current_indexes[i + 0],
+                           current_indexes[i + 1],
+                           current_indexes[i + 2]) <
+                    mean_z(current_vertexes,
+                           current_indexes[j + 0],
+                           current_indexes[j + 1],
+                           current_indexes[j + 2]))
+                {
+                    std::swap(current_indexes[i + 0], current_indexes[j + 0]);
+                    std::swap(current_indexes[i + 1], current_indexes[j + 1]);
+                    std::swap(current_indexes[i + 2], current_indexes[j + 2]);
+                }
+            }
+        }
 
         triangle_render.clear({ 255, 255, 255 });
-        triangle_render.draw_triangles(vertexes, indexes);
+        triangle_render.draw_triangles(current_vertexes, current_indexes);
 
         SDL_Surface* bitmapSurface = SDL_CreateSurfaceFrom(
             pixels, width, height, pitch, SDL_PIXELFORMAT_RGB24);
@@ -205,6 +278,9 @@ int main()
         SDL_RenderPresent(renderer);
 
         SDL_DestroyTexture(bitmapTex);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000 / 60));
+        phi += dPhi / 5;
     }
 
     SDL_DestroyRenderer(renderer);
