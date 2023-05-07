cad47236a865272529f0fa03e31d39698e4f4337
Stepanov Igor <ygrik2003@yandex.ru>
2023-05-07 13:22:27 +0300

    add rotate for sphere

--- 04-2-render-opengl/game.cpp
+++ 04-2-render-opengl/game.cpp
@@ -1,20 +1,42 @@
 #include "game.h"
 #include <cassert>
 
+#include <iostream>
+
 sphere::sphere(uint r)
+{
+    set_rotate(0, 0, 0);
+    set_scale(1, 1, 1);
+    set_translate(0, 0, 0);
+    radius = r;
+    calculate();
+}
+
+void sphere::calculate()
 {
     int    index = 0;
     double theta = 0, phi;
 
-    _vertexes.push_back(get_sphere(0, 0, r));
-    for (int theta_counter = 1; theta_counter <= M_PI / d2; theta_counter++)
+    _vertexes.resize(0);
+    indexes.resize(0);
+
+    _vertexes.push_back(get_sphere(0, 0, radius));
+    for (int theta_counter = 0; theta_counter <= M_PI / d2; theta_counter++)
     {
         phi = 0;
         for (int phi_counter = 0; phi_counter <= 2 * M_PI / d1; phi_counter++)
         {
-            _vertexes.push_back(get_sphere(phi, theta, r));
+            if (theta_counter != 0)
+                _vertexes.push_back(get_sphere(phi, theta, radius));
 
-            if (theta_counter < static_cast<int>(M_PI / d2))
+            if (theta_counter == 0)
+            {
+                indexes.push_back(get_index(phi_counter, theta_counter));
+                indexes.push_back(get_index(phi_counter, theta_counter + 1));
+                indexes.push_back(
+                    get_index(phi_counter + 1, theta_counter + 1));
+            }
+            else if (theta_counter < static_cast<int>(M_PI / d2))
             {
                 indexes.push_back(get_index(phi_counter, theta_counter));
                 indexes.push_back(get_index(phi_counter + 1, theta_counter));
@@ -37,7 +59,7 @@ sphere::sphere(uint r)
         }
         theta += d2;
     }
-    _vertexes.push_back(get_sphere(0, M_PI, r));
+    _vertexes.push_back(get_sphere(0, M_PI, radius));
     count_triangles = indexes.size() / 3;
 }
 
@@ -61,14 +83,14 @@ uint sphere::get_index(int phi_c, int theta_c)
     }
     else if (theta_c == static_cast<int>(M_PI / d2) + 1)
     {
-        return (static_cast<int>(M_PI / d2) + 1) *
-                   (static_cast<int>(2 * M_PI / d1)) +
+        return (static_cast<int>(M_PI / d2)) *
+                   (static_cast<int>(2 * M_PI / d1) + 1) +
                1;
     }
     else
     {
         return 1 + (theta_c - 1) * static_cast<int>(2 * M_PI / d1 + 1) +
-               phi_c % static_cast<int>(2 * M_PI / d1);
+               phi_c % static_cast<int>(2 * M_PI / d1 + 1);
     }
 };
 vertex sphere::get_sphere(double phi, double theta, uint r)
@@ -81,46 +103,47 @@ vertex sphere::get_sphere(double phi, double theta, uint r)
 
 void object::set_rotate(float alpha, float beta, float gamma)
 {
-    mat_rotate[0][0] = cos(alpha) * cos(beta);
-    mat_rotate[0][1] =
+    tr_obj.rotate[0][0] = cos(alpha) * cos(beta);
+    tr_obj.rotate[0][1] =
         cos(alpha) * sin(beta) * sin(gamma) - sin(alpha) * cos(gamma);
-    mat_rotate[0][2] =
+    tr_obj.rotate[0][2] =
         cos(alpha) * sin(beta) * cos(gamma) + sin(alpha) * sin(gamma);
 
-    mat_rotate[1][0] = sin(alpha) * cos(beta);
-    mat_rotate[1][1] =
+    tr_obj.rotate[1][0] = sin(alpha) * cos(beta);
+    tr_obj.rotate[1][1] =
         sin(alpha) * sin(beta) * sin(gamma) + cos(alpha) * cos(gamma);
-    mat_rotate[1][2] =
+    tr_obj.rotate[1][2] =
         sin(alpha) * sin(beta) * cos(gamma) - cos(alpha) * sin(gamma);
 
-    mat_rotate[2][0] = -sin(beta);
-    mat_rotate[2][1] = cos(beta) * sin(gamma);
-    mat_rotate[2][2] = cos(beta) * cos(gamma);
-
-    mat_rotate[3][3] = 1;
+    tr_obj.rotate[2][0] = -sin(beta);
+    tr_obj.rotate[2][1] = cos(beta) * sin(gamma);
+    tr_obj.rotate[2][2] = cos(beta) * cos(gamma);
 }
 void object::set_translate(float dx, float dy, float dz)
 {
-    mat_translate[3][0] = dx;
-    mat_translate[3][1] = dy;
-    mat_translate[3][2] = dz;
+    tr_obj.translate[0] = dx;
+    tr_obj.translate[1] = dy;
+    tr_obj.translate[2] = dz;
 }
 void object::set_scale(float scale_x, float scale_y, float scale_z)
 {
-    mat_scale[0][0] = scale_x;
-    mat_scale[1][1] = scale_y;
-    mat_scale[2][2] = scale_z;
+    tr_obj.scale[0] = scale_x;
+    tr_obj.scale[1] = scale_y;
+    tr_obj.scale[2] = scale_z;
 }
 void object::set_discretization(float d_phi, float d_theta)
 {
     d1 = d_phi;
     d2 = d_theta;
+    calculate();
 }
 
 void object::render(engine* _engine)
 {
     for (uint i = 0; i < size(); i++)
-        _engine->render_triangle(get_triangle(i));
+    {
+        _engine->render_triangle(get_triangle(i), tr_obj);
+    }
 }
 
 sphere_game::sphere_game(engine* e)
@@ -139,7 +162,7 @@ void sphere_game::remove_object(size_t index)
     _objects.erase(_objects.begin() + index);
 }
 
-object sphere_game::get_object(size_t index)
+object& sphere_game::get_object(size_t index)
 {
     assert(index < _objects.size());
     return _objects[index];
