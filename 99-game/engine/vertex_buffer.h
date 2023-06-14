ca28f57a81b7cfd30803094182bff1c022a751d9
Stepanov Igor <ygrik2003@yandex.ru>
2023-06-14 22:19:05 +0300

    end refactor

--- 99-game/engine/vertex_buffer.h
+++ 99-game/engine/vertex_buffer.h
@@ -9,40 +9,11 @@ class vertex_buffer
 {
 public:
     vertex_buffer(const triangle<vertex_type>* tri, std::size_t n);
+    vertex_buffer(const vertex_type* vert, std::size_t n);
+    ~vertex_buffer();
 
-    vertex_buffer(const vertex_type* vert, std::size_t n)
-        : count(n)
-    {
-        glGenBuffers(1, &gl_handle);
-        GL_CHECK_ERRORS()
-
-        bind();
-
-        GLsizeiptr size_in_bytes =
-            static_cast<GLsizeiptr>(n * sizeof(vertex_type));
-
-        glBufferData(GL_ARRAY_BUFFER, size_in_bytes, vert, GL_STATIC_DRAW);
-        GL_CHECK_ERRORS()
-    }
-    // vertex_buffer(const vertex3d_colored* vert, std::size_t n);
-    // vertex_buffer(const vertex3d_textured* vert, std::size_t n);
-    // vertex_buffer(const vertex3d* vert, std::size_t n);
-
-    ~vertex_buffer()
-    {
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        GL_CHECK_ERRORS()
-        glDeleteBuffers(1, &gl_handle);
-        GL_CHECK_ERRORS()
-    }
-
-    void bind() const
-    {
-        glBindBuffer(GL_ARRAY_BUFFER, gl_handle);
-        GL_CHECK_ERRORS()
-    }
-
-    uint32_t size() const { return count; }
+    void     bind() const;
+    uint32_t size() const;
 
 private:
     uint32_t gl_handle{ 0 };
